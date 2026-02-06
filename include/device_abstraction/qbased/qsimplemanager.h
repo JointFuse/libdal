cf7a89419a29cf36972921e921bb7f0d01f87271
JointFuse <JointFuse@yandex.ru>
2026-02-06 16:43:24 +0300

    Deprecate QSimpleManager due to use of unsafe callback mechanism

--- include/device_abstraction/qbased/qsimplemanager.h
+++ include/device_abstraction/qbased/qsimplemanager.h
@@ -1,12 +1,12 @@
 #ifndef QSIMPLEMANAGER_H
 #define QSIMPLEMANAGER_H
 
+#include "../asynch/queuemanager.h"
+#include "../dalcore.h"
+
 #include <QObject>
 
-#include "../dalcore.h"
-#include "../asynch/queuemanager.h"
-
-namespace dal { ////////////////////////////////////////////////////////////////
+namespace dal {    ////////////////////////////////////////////////////////////////
 /**
  * @brief The QBaseManager class
  */
@@ -14,38 +14,37 @@ class QBaseManager : public QObject, public AsynchRespondManager
 {
     Q_OBJECT
 
-public slots:
+  public slots:
     void invocationSlot();
 
-public:
-    QBaseManager(std::unique_ptr<DeviceDriver> executor,
-                 SimpleQueue::handle_t queue);
+  public:
+    QBaseManager( std::unique_ptr<DeviceDriver> executor, SimpleQueue::handle_t queue );
     ~QBaseManager();
 
-private:
+  private:
     DAL_DECLARE_PIMPL
-
 };
 /**
  * @brief The QSimpleManager class
  */
-class QSimpleManager : public QBaseManager
+class [[deprecated(
+    "This class is deprecated. Using of it may lead to undefined behavior. Strongly recomend to "
+    "use QPromiseManager wich not using unsafe callback mechanism QMetaObject::invokeMethod." )]]
+QSimpleManager : public QBaseManager
 {
     Q_OBJECT
 
-public:
-    QSimpleManager(std::unique_ptr<DeviceDriver> executor,
-                   SimpleQueue::handle_t queue);
+  public:
+    QSimpleManager( std::unique_ptr<DeviceDriver> executor, SimpleQueue::handle_t queue );
     ~QSimpleManager();
 
-protected:
-    void processAction(AbstractAction::actionHandle_t&) override;
-    bool takeActionFromQueue(AbstractAction::actionHandle_t&) override;
-    std::function<void(AbstractResponse::responseHandle_t)> responseSender() override;
+  protected:
+    void processAction( AbstractAction::actionHandle_t& ) override;
+    bool takeActionFromQueue( AbstractAction::actionHandle_t& ) override;
+    std::function<void( AbstractResponse::responseHandle_t )> responseSender() override;
 
-private:
+  private:
     DAL_DECLARE_PIMPL
-
 };
 /**
  * @brief The QPromiseManager class
@@ -54,19 +53,17 @@ class QPromiseManager : public QBaseManager
 {
     Q_OBJECT
 
-public:
-    QPromiseManager(std::unique_ptr<DeviceDriver> executor,
-                    SimpleQueue::handle_t queue);
+  public:
+    QPromiseManager( std::unique_ptr<DeviceDriver> executor, SimpleQueue::handle_t queue );
     ~QPromiseManager();
 
-protected:
-    std::function<void(AbstractResponse::responseHandle_t)> responseSender();
+  protected:
+    std::function<void( AbstractResponse::responseHandle_t )> responseSender();
 
-private:
+  private:
     DAL_DECLARE_PIMPL
-
 };
 
-} //////////////////////////////////////////////////////////////////////////////
+}    // namespace dal
 
-#endif // QSIMPLEMANAGER_H
+#endif    // QSIMPLEMANAGER_H
